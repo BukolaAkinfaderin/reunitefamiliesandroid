41137f4c85c79492b70bb1a467203f509b1b38a6
David Milliner <david.milliner@inwebo.com>
2018-06-30 17:22:32 -0700

    get data of database

--- app/src/main/java/com/reunitefamilies/reunitefamilies/signIn/SignInActivity.kt
+++ app/src/main/java/com/reunitefamilies/reunitefamilies/signIn/SignInActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -11,8 +14,8 @@ import com.reunitefamilies.reunitefamilies.R
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
+import com.reunitefamilies.reunitefamilies.adapter.Adapter
 import com.reunitefamilies.reunitefamilies.models.Children
-import android.widget.Toast
 
 class SignInActivity: AppCompatActivity() {
     companion object {
@@ -25,8 +28,10 @@ class SignInActivity: AppCompatActivity() {
     private lateinit var presenter: Contract.Presentation
     private lateinit var coordinatior: Contract.Coordination
 
+    private lateinit var list: RecyclerView
+    private var adapter: Adapter? = null
+
     private lateinit var mDatabaseReference: DatabaseReference
-    private lateinit var mPostReference: DatabaseReference
 
     fun provide(coordinatior: Contract.Coordination, presenter: Contract.Presentation){
         this.presenter = presenter
@@ -39,41 +44,47 @@ class SignInActivity: AppCompatActivity() {
 
         Dependencies().inject(this)
 
-        mDatabaseReference = FirebaseDatabase.getInstance().reference
+        list = findViewById(R.id.list)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        list.layoutManager = layoutManager
 
+        adapter = Adapter()
+        list.adapter = adapter
+
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
         mDatabaseReference.child("children").child(mDatabaseReference.push().key!!).setValue(
                 Children("origin", "dob", "first_name", "last_name", "location", 5, "pending" ))
 
-        // Attach a listener to read the data at our posts reference
-        mDatabaseReference.addValueEventListener(object : ValueEventListener {
+//        // Attach a listener to read the data at our posts reference
+//        mDatabaseReference.addValueEventListener(object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                val post = dataSnapshot.getValue<Children>(Children::class.java)
+//                Log.d("Testing", "Post is: " + post as Children)
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                println("The read failed: " + databaseError.code)
+//            }
+//        })
+
+        val childrenList = ArrayList<Children>()
+        val database = FirebaseDatabase.getInstance().reference
+
+
+        database.child("children").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val post = dataSnapshot.getValue<Children>(Children::class.java)
-                Log.d("Testing", "Post is: " + post as Children)
+                for (childrenDataSnapshot in dataSnapshot.children) {
+                    val child = childrenDataSnapshot.getValue(Children::class.java)
+                    childrenList.add(child!!)
+                    adapter?.childRow(child.first_name!!, child.last_name!!)
+                }
+
+                Log.d("Testing", ">>>>> children List is: " + childrenList)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
                 println("The read failed: " + databaseError.code)
             }
         })
-
-        mPostReference = FirebaseDatabase.getInstance().getReference().child("children")
-
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get Post object and use the values to update the UI
-                val post = dataSnapshot.getValue<Children>(Children::class.java)
-                Log.d("Testing", "Post is: " + post as Children)
-
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("Testing", "loadPost:onCancelled", databaseError.toException())
-
-                Toast.makeText(this@SignInActivity, "Failed to load post.", Toast.LENGTH_SHORT).show()
-
-            }
-        }
-        mPostReference.addValueEventListener(postListener)
     }
 }
