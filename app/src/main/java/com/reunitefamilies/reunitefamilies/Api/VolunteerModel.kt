6bcc6267be3f5f3bc1661c8e03c82738a63119d7
bukolajandus <bukolajobs@yahoo.com>
2018-12-01 16:18:51 -0800

    updated volunteer model

--- app/src/main/java/com/reunitefamilies/reunitefamilies/Api/VolunteerModel.kt
+++ app/src/main/java/com/reunitefamilies/reunitefamilies/Api/VolunteerModel.kt
@@ -2,11 +2,11 @@ package com.reunitefamilies.reunitefamilies.Api
 
 import java.io.Serializable
 
-class VolunteerModel: Serializable  {
+data class VolunteerModel (
 
-    var id: String? = null
-    var name: String? = null
-    var location: String? = null
-    var firebase_id: String? = null
-    var languages: List<String>? = null
-}
+    val id: String,
+    val name: String,
+    val location: String? = null,
+    val firebase_id: String,
+    val languages: List<String>
+) : Serializable
